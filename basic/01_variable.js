e298256de45cef1a0e58e703f8632ffbdece581e
Itspoision <89988008+Itspoision@users.noreply.github.com>
2024-08-15 08:56:53 +0000

    variable config done

---
+++ basic/01_variable.js
@@ -0,0 +1,11 @@
+const accountid = 41551;
+let accountName = "prachi goyal"
+let accountEmail = "prachi@gamil.com"
+var accountState = "punjab"
+var accountcity;
+// accountid = 234; //const can not be change
+/* 
+never use var 
+because of issue in block and function scope.
+*/
+console.table([accountid,accountEmail,accountName,accountState,accountcity]);
