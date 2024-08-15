1f0db6beef5a6ce86f209463c9deaef4d8569c4c
Itspoision <89988008+Itspoision@users.noreply.github.com>
2024-08-15 09:37:30 +0000

    datatype and conversion

---
+++ basic/03_conversion.js
@@ -0,0 +1,18 @@
+let score = undefined
+/*
+  33=>33
+  "33"=>33  
+  "33abc" => NaN
+   null => 0
+   true => 1
+   "prachi" => NaN
+*/
+let value = Number(score)
+//console.log(typeof value);
+//console.log(value);
+
+let str1 = "hello "
+let str2 = "Prachi"
+console.log(str1+str2)
+console.log(`hello ${str2} prachi ${str2} `);
+
